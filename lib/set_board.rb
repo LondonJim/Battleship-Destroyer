7266a35e00a43a1cb5b6ba9b9d637dc86a53f642
James Sutherland <jrsutherland78@googlemail.com>
2019-01-08 23:36:53 +0000

    add #game_board to place ships on

--- lib/set_board.rb
+++ lib/set_board.rb
@@ -1,3 +1,27 @@
 class SetBoard
 
+  attr_reader :game_board
+
+  def initialize
+    @game_board = []
+  end
+
+  def create_board(width = 12, height = 12)
+    col_pos = 1
+    row_pos = 1
+    board_row = []
+
+    while col_pos <= width do
+      while row_pos <= height do
+        board_row.push(" ")
+        row_pos += 1
+      end
+      @game_board.push(board_row)
+      col_pos += 1
+      row_pos = 1
+      board_row = []
+    end
+    @game_board
+  end
+
 end
--- spec/set_board_spec.rb
+++ spec/set_board_spec.rb
@@ -2,8 +2,35 @@ require 'set_board'
 
 describe SetBoard do
 
-  it 'creates a new SetBoard class' do
-    set_board = SetBoard.new
+  before(:each) do
+    @set_board = SetBoard.new
+  end
+
+  it 'creates a new instantiation of SetBoard class' do
+    expect(@set_board).to be_instance_of(SetBoard)
+  end
+
+  it 'instantiates with an empty @game_board array' do
+    expect(@set_board.game_board).to eq([])
+  end
+
+  describe "#create_board" do
+
+    it 'creates a 12x12 whitespace board' do
+      expect(@set_board.create_board).to eq([[" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+                                             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+                                             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+                                             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+                                             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+                                             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+                                             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+                                             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+                                             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+                                             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+                                             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+                                             [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]])
+    end
+
   end
 
 end
