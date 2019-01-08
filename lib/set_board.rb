f591fc0d666684b457928c7ae19e5b9b82ab940b
James Sutherland <jrsutherland78@googlemail.com>
2019-01-08 23:39:30 +0000

    change width and height of game board to instantiate on creation of new class

--- lib/set_board.rb
+++ lib/set_board.rb
@@ -2,17 +2,19 @@ class SetBoard
 
   attr_reader :game_board
 
-  def initialize
+  def initialize(width = 12, height = 12)
+    @width = width
+    @height = height
     @game_board = []
   end
 
-  def create_board(width = 12, height = 12)
+  def create_board()
     col_pos = 1
     row_pos = 1
     board_row = []
 
-    while col_pos <= width do
-      while row_pos <= height do
+    while col_pos <= @width do
+      while row_pos <= @height do
         board_row.push(" ")
         row_pos += 1
       end
